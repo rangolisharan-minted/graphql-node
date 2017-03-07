01ac6b22985ad0c4b9611224023ef06e8a6ea821
Rangoli Sharan <rangoli@Rangolis-MacBook-Pro.local>
2017-03-06 20:09:30 -0800

    WIP

--- docker_bash.sh
+++ docker_bash.sh
@@ -6,4 +6,4 @@ DOCKERHOST=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" |\
  grep -v 127.0.0.1 |\
  awk '{ print $2 }' |\
  cut -f2 -d: | head -n1)\
- docker-compose run --rm chat /bin/bash
+ docker-compose run --rm graphql /bin/bash
